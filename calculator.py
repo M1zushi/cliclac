d008e615de6d98c1ed48f16b2927db49b74cb02a
M1zushi <usersama2006@gmail.com>
2021-04-18 22:57:43 +0400

    Just a few starting steps. Will work on more tomorrow.

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import math
+
+# Need to add active loop
+
+print '1. Add'
+print '2. Subtract'
+print '3. Multiply'
+print '4. Divide'
+# Temporarily there will only be the basic 4 operators
+print '\n0. Off'
+
+op = input('Operation(Num 0-4): ')
+
+if op == 1:
+    print('Placeholder')
+
+elif op == 2:
+    print('Placeholder')
+
+elif op == 3:
+    print('Placeholder')
+
+elif op == 4:
+    print('Placeholder')
+
+elif op == 0:
+    print('O F F')
+    # Will set this up to break loop
